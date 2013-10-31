dc05fcd519509577ba9fa767f6262f606a8dcd37
devDark <dimapv@gmail.com>
2013-10-31 18:29:11 +0100

    Update scmquery.js

--- scmquery.js
+++ scmquery.js
@@ -9,70 +9,84 @@
         }(window);
         var SCMQuery = undefined;
         /* Init Core */
-        SCMQuery = new function SCMQuery() {
-            var self = this;
-            self.GS = new function SCMQuery_GS() {
-                this.__name__ = "SCMQuery - Garbage storage";
-            };
-            self.win = window;
-            self.doc = this.win.document;
-            self.loc = this.win.location || this.doc.location;
-            self.nav = this.win.navigator;
-            self.usrag = this.nav.userAgent;
-            self.ua = this.usrag.toLowerCase();
-            self.appver = this.nav.appVersion;
-            self.av = this.appver.toLowerCase();
-            self.screen = this.win.screen;
-            if (typeof (self.win.addEventListener) != "undefined") {
-                self.addEvent = function (elem, type, handler, type2) {
-                    type2 = undefined;
+        var SCMQueryProto = new function SCMQuery() {
+            var self = this,
+            __name__ = 'SCMQuery',
+            __version__ = '1.0.3',
+            __build__ = 'nonstable',
+            __info__ = function() {
+                return {'__name__' : __name__, '__version__' : __version__, '__build__' : __build__};
+            },
+            GS = function SCMQuery_GS() {
+                var __name__ = 'SCMQuery - Garbage storage';
+                this.__name__ = function() {
+                    return {'__name__' : __name__};
+                };
+            },
+            win = window,
+            doc = win.document,
+            loc = win.location || win.location,
+            nav = win.navigator,
+            usrag = nav.userAgent,
+            ua = usrag.toLowerCase(),
+            appver = nav.appVersion,
+            av = appver.toLowerCase(),
+            screen = win.screen,
+            addEvent = undefined,
+            removeEvent = undefined,
+            extendfn = function(extend, extendbody) {
+                if(!self[extend]) {
+                    self[extend] = extendbody;
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            docReadyState = false,
+            docReadyStack = new Array(),
+            docReady = undefined,
+            docReadyExec = undefined,
+            CurrentOS = undefined;
+            extendfn('extend', extendfn);
+            extendfn('extendfn', extendfn);
+            extendfn('GS', new GS);
+            if (typeof(self.win.addEventListener) !== 'undefined') {
+                addEvent = function (elem, type, handler) {
                     elem.addEventListener(type, handler, false);
                 };
-                self.removeEvent = function (elem, type, handler, type2) {
-                    type2 = undefined;
+                removeEvent = function (elem, type, handler) {
                     elem.removeEventListener(type, handler, false);
                 };
-            } else if (typeof (self.win.attachEvent) != "undefined") {
-                self.addEvent = function (elem, type, handler, type2) {
-                    if (typeof (type2) != "string") {
-                        type = undefined;
-                        type = type2;
-                    }
+            } else if (typeof(self.win.attachEvent) !== 'undefined') {
+                self.addEvent = function (elem, type, handler) {
                     elem.attachEvent("on" + type, handler);
                 };
-                self.removeEvent = function (elem, type, handler, type2) {
-                    if (typeof (type2) != "string") {
-                        type = undefined;
-                        type = type2;
-                    }
+                removeEvent = function (elem, type, handler) {
                     elem.detachEvent("on" + type, handler);
                 };
             }
-            var docReadyState = false;
-            self.addEvent(self.doc, "DOMContentLoaded", function () {
+            extendfn('addEvent', addEvent);
+            extendfn('removeEvent', removeEvent);
+            addEvent(self.doc, "DOMContentLoaded", function () {
                 docReadyState = true;
                 self.removeEvent(self.doc, "DOMContentLoaded", arguments.callee, "readystatechange");
             }, "readystatechange");
-            var drlist = new Array();
-            self.docReady = function (fn) {
-                if (docReadyState == true) {
-                    if (typeof (fn) == "function") {
+            docReady = function (fn) {
+                if (docReadyState === true) {
+                    if (typeof(fn) === 'function') {
                         fn();
                     } else {
                         (new Function(fn))();
                     }
                 } else {
-                    drlist.push(fn);
+                    docReadyStack.push(fn);
                 }
             };
-            var drlistid = self.win.setInterval(function () {
-                if (docReadyState == true || self.doc.readyState == "complete") {
-                    if (docReadyState != true) {
+            docReadyExec = function () {
+                if (docReadyState === true || self.doc.readyState === 'complete') {
+                    if (docReadyState !== true) {
                         docReadyState = true;
                     }
-                    if (drlist.length == 0) {
-                        self.win.clearInterval(drlistid);
-                    } else {
                         var drlisttmp = drlist.shift();
                         if (typeof (drlisttmp) == "function") {
                             drlisttmp();
@@ -81,20 +95,21 @@
                         }
                     }
                 }
-            }, 10);
-            self.GetCurrentOS = (function () {
+            };
+            CurrentOS = (function CurrentOS() {
                 var curos = "UnknownOS";
                 if (self.ua.indexOf("win") != -1 || self.av.indexOf("win") != -1) {
                     curos = "windows";
                 } else if (self.ua.indexOf("mac") != -1 || self.av.indexOf("mac") != -1) {
                     curos = "osx";
                 } else if (self.ua.indexOf("x11") != -1 || self.av.indexOf("x11") != -1 || self.ua.indexOf("linux") != -1 || self.av.indexOf("linux") != -1) {
-                    curos = "linux";
+                    curos = "unix";
                 } else if (self.ua.indexOf("brew") != -1 || self.av.indexOf("brew") != -1 || self.ua.indexOf("obigo") != -1 || self.av.indexOf("obigo") != -1) {
                     curos = "brew";
                 }
                 return curos;
             })();
+            extendfn('CurrentOS', CurrentOS);
             self.ajax = (function () {
                 var xhr = null;
                 try {
